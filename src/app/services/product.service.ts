ad0e3e7253baf448e6d4f3fb510689d889a66f58
ulascanb58 <ulascanb58@gmail.com>
2021-05-29 15:53:48 +0300

    errors

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -23,5 +23,8 @@ export class ProductService {
     let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product)
+  }
 
 }
